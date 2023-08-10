429db19b98e376fa3cfdf9a794558c31170e0022
Addendumexpired <130726953+Addendumexpired@users.noreply.github.com>
2023-08-10 19:00:34 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,11 +1,24 @@
+import http.server
+import http.client
 
-from http.server import BaseHTTPRequestHandler
- 
-class handler(BaseHTTPRequestHandler):
- 
+class ReverseProxyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        # 创建到目标服务器的连接
+        target = http.client.HTTPConnection("www.baidu.com")
+        target.request("GET", self.path)
+
+        # 获取目标服务器的响应
+        response = target.getresponse()
+
+        # 设置本地服务器的响应
+        self.send_response(response.status)
+        for header, value in response.getheaders():
+            self.send_header(header, value)
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+        self.copyfile(response, self.wfile)
+
+if __name__ == '__main__':
+    server_address = ('', 8000)
+    httpd = http.server.HTTPServer(server_address, ReverseProxyHandler)
+    print('Starting reverse proxy server on port 8000...')
+    httpd.serve_forever()
