72ebc806b8861d7c3e1a4dc95f64295ead6ba9c9
Addendumexpired <130726953+Addendumexpired@users.noreply.github.com>
2023-08-10 19:26:00 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -6,17 +6,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-        test = """<!DOCTYPE html>
-<html>
-<head>
-    <title>简单的HTML示例</title>
-</head>
-<body>
-    <h1>欢迎来到我的网页</h1>
-    <p>这是一个简单的HTML示例，用于演示基本的HTML标记。</p>
-    <a href="https://www.example.com">访问示例网站</a>
-</body>
-</html>
-"""
+        test = """fff"""
         self.wfile.write(test.encode('utf-8'))
         return
