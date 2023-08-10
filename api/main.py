873aa905dce3e1e5ffc2296829525c07c3b7763d
Addendumexpired <130726953+Addendumexpired@users.noreply.github.com>
2023-08-10 19:04:56 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,20 +1,10 @@
-import http.client
-import json
-
-def handler(event, context):
-    # 创建到目标服务器的连接
-    target = http.client.HTTPConnection("www.baidu.com")
-    target.request("GET", event['path'])
-
-    # 获取目标服务器的响应
-    response = target.getresponse()
-    response_headers = dict(response.getheaders())
-
-    # 构建返回给客户端的响应
-    proxy_response = {
-        "statusCode": response.status,
-        "headers": response_headers,
-        "body": response.read().decode('utf-8')
-    }
-
-    return proxy_response
+from http.server import BaseHTTPRequestHandler
+ 
+class handler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
