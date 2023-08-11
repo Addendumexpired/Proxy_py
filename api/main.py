8c22d07b18b5f401285d90a43e17099aa6ad76c5
Addendumexpired <130726953+Addendumexpired@users.noreply.github.com>
2023-08-11 12:38:44 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -7,7 +7,7 @@ import re
 class handler(BaseHTTPRequestHandler):
     
 
-    
+   
 
     
     def do_GET(self):
@@ -17,36 +17,25 @@ class handler(BaseHTTPRequestHandler):
         } 
 
 
-        target_url = "https://github.com/"
-        raw_url = "https://raw.githubusercontent.com/" #raw文件
-        allfile_url = "https://codeload.github.com/" #所有文件
-
-
-        match_raw = re.match(r"/([^/]+)/([^/]+)/raw/([^/]+)/([^/]+)", self.path) #单个文件都是raw
-        match_allfile =re.match(r"/([^/]+)/([^/]+)/archive/refs/heads/([^/]+).zip", self.path)
         
 
 
 
-        if match_raw:
-            response = requests.get(raw_url + f"/{match_raw.group(1)}/{match_raw.group(2)}/{match_raw.group(3)}/{match_raw.group(4)}", headers=headers)
-        elif match_allfile:
-            response = requests.get(allfile_url + f"/{match_allfile.group(1)}/{match_allfile.group(2)}/zip/refs/heads/{match_allfile.group(3)}", headers=headers)
-        else:
-            response = requests.get(target_url + self.path, headers=headers)
+        
+        response = requests.get("https://codeload.github.com/pojiezhiyuanjun/freev2/zip/refs/heads/master", headers=headers)
 
         
 
-        # 获取响应内容和状态码
-        response_content = response.content
-        response_status = response.status_code
 
-        
-        self.send_response(response_status)
 
-            
-        self.send_header("Content-type", response.headers["Content-type"])
+        zip_filename = "master.zip"
+        self.send_response(200)
+        self.send_header("Content-type", "application/zip")
+        self.send_header("Content-Disposition", f"attachment; filename={zip_filename}")
         self.end_headers()
-        self.wfile.write(response_content)
+
+        self.send_file_in_chunks(response.content, zip_filename)
+
+        self.wfile.write(response.content)
 
         return
