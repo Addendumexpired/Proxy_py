6be531e476b369f835413bcfe4272b52a26c44cc
Addendumexpired <130726953+Addendumexpired@users.noreply.github.com>
2023-08-11 16:53:18 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -6,34 +6,22 @@ import re
 
 class handler(BaseHTTPRequestHandler):
     
-
-    
-
-    
     def do_GET(self):
         headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-    "Accept": "application/zip"
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
         } 
 
 
         target_url = "https://codeload.github.com/pojiezhiyuanjun/freev2/zip/refs/heads/master"
        
-       
-
-
            
         response = requests.get(target_url, headers=headers, stream=True)
         
         self.send_response(200)
-
-        for k,v in response.headers.items():
-          
-            self.send_header(k,v)
-        
-
-
-        
-        self.wfile.write(response.content[1:10])
+        self.send_header("Content-Disposition","attachment; filename=freev2-master.zip")
+        self.send_header("Content-Type","application/zip")
+        self.end_headers()
+        self.wfile.write(response.content)
 
         return
