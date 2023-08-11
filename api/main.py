2e9928b4f0542b6bcacf2e29f8617c0e581896c1
Addendumexpired <130726953+Addendumexpired@users.noreply.github.com>
2023-08-11 13:10:03 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,32 +1,37 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import http.client
 import requests
+from bs4 import BeautifulSoup
+
 
 class handler(BaseHTTPRequestHandler):
+    
 
+
+    
     def do_GET(self):
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
         }
 
-      
-        url1 = "https://github.com/pojiezhiyuanjun/freev2/archive/refs/heads/master.zip"
-        response1 = requests.get(url1, allow_redirects=False)
 
-        if response1.status_code == 302 and 'Location' in response1.headers:
-            # 获取重定向的位置
-            redirect_url = response1.headers['Location']
+        target_url = "https://github.com/"
+       
+        # 发起 GET 请求
+        response = requests.get(target_url + self.path, headers=headers)
 
-            # 请求网址2
-            response2 = requests.get(redirect_url, headers=headers)
+        # 获取响应内容和状态码
+        response_content = response.content
+        response_status = response.status_code
+
+      #https://github.com/pojiezhiyuanjun/freev2/archive/refs/heads/master.zip 
+
+        self.send_response(response_status)
 
             
-        elf.send_response(200)
-
-            
-        self.send_header("Content-type", response1.headers["Content-type"])
+        self.send_header("Content-type", response.headers["Content-type"])
         self.end_headers()
-        self.wfile.write(response1)
+        self.wfile.write(response_content)
 
-            
         return
