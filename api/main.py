9c6e5c36dd2c871e6a60f3e47daa922b6f328bd9
Addendumexpired <130726953+Addendumexpired@users.noreply.github.com>
2023-08-11 13:01:24 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -9,17 +9,32 @@ class handler(BaseHTTPRequestHandler):
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
         }
 
-        response = requests.get("https://codeload.github.com/pojiezhiyuanjun/freev2/zip/refs/heads/master", headers=headers)
+        # 请求网址1，获取重定向的位置
+        url1 = "https://github.com/pojiezhiyuanjun/freev2/archive/refs/heads/master.zip"
+        response1 = requests.get(url1, allow_redirects=False)
 
-        zip_filename = "master.zip"
-        self.send_response(200)
-        self.send_header("Content-type", "application/zip")
-        self.send_header("Content-Disposition", f"attachment; filename={zip_filename}")
-        self.send_header("Content-Length", len(response.content))
-        self.end_headers()
+        if response1.status_code == 302 and 'Location' in response1.headers:
+            # 获取重定向的位置
+            redirect_url = response1.headers['Location']
 
-        chunk_size = 8192  # Adjust the chunk size as needed
-        for i in range(0, len(response.content), chunk_size):
-            self.wfile.write(response.content[i:i + chunk_size])
+            # 请求网址2
+            response2 = requests.get(redirect_url, headers=headers)
 
-        return
+            if response2.status_code == 200:
+                self.send_response(200)
+                self.send_header("Content-type", "application/zip")
+                self.send_header("Content-Disposition", "attachment; filename=master.zip")
+                self.send_header("Content-Length", len(response2.content))
+                self.end_headers()
+
+                chunk_size = 8192  # Adjust the chunk size as needed
+                for i in range(0, len(response2.content), chunk_size):
+                    self.wfile.write(response2.content[i:i + chunk_size])
+            else:
+                self.send_response(500)
+                self.end_headers()
+                self.wfile.write(b"Error fetching content from URL2")
+        else:
+            self.send_response(500)
+            self.end_headers()
+            self.wfile.write(b"Error fetching content from URL1")
