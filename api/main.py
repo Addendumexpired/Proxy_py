cde6ff90774b26a4fb2453c1cbdce4ce18176fa8
Addendumexpired <130726953+Addendumexpired@users.noreply.github.com>
2023-08-11 16:40:07 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -13,41 +13,29 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+    "Accept": "application/zip"
         } 
 
 
-        target_url = "https://github.com/"
-        raw_url = "https://raw.githubusercontent.com/" #raw文件
-        allfile_url = "https://codeload.github.com/" #所有文件
+        target_url = "https://codeload.github.com/pojiezhiyuanjun/freev2/zip/refs/heads/master"
+       
+       
 
 
-        match_raw = re.match(r"/([^/]+)/([^/]+)/raw/([^/]+)/([^/]+)", self.path) #单个文件都是raw
-        match_allfile =re.match(r"/([^/]+)/([^/]+)/archive/refs/heads/([^/]+).zip", self.path)
+           
+        response = requests.get(target_url, headers=headers, stream=True)
         
+        self.send_response(200)
 
-
-
-        if match_raw:
-            response = requests.get(raw_url + f"/{match_raw.group(1)}/{match_raw.group(2)}/{match_raw.group(3)}/{match_raw.group(4)}", headers=headers)
-        elif match_allfile:
-            response = requests.get(allfile_url + f"/{match_allfile.group(1)}/{match_allfile.group(2)}/zip/refs/heads/{match_allfile.group(3)}", headers=headers)
-        else:
-            response = requests.get(target_url + self.path, headers=headers)
-
+        for k,v in response.headers.items():
+          
+            self.send_header(k,v)
         
+        self.send_header('', '')
 
-        # 获取响应内容和状态码
-        response_content = response.content
-        response_status = response.status_code
 
-        
-        self.send_response(response_status)
-
-            
-        self.send_header("Content-type", response.headers["Content-type"])
         self.end_headers()
-        self.wfile.write(response_content)
+        for chunk in response.iter_content(chunk_size=1024):  # Adjust the chunk size as needed
+            self.wfile.write(chunk)
 
         return
-        
