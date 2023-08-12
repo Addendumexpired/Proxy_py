cef670f076aed3810b546dff9f01959463148996
Addendumexpired <130726953+Addendumexpired@users.noreply.github.com>
2023-08-12 14:49:37 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -8,14 +8,23 @@ class handler(BaseHTTPRequestHandler):
     
     
     def do_GET(self):
-        target_url ="https://codeload.github.com/pojiezhiyuanjun/freev2/zip/refs/heads/master"
-        response = urllib.request.urlopen(target_url)
+        headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+        } 
+
+
+        target_url = "https://github.com/pojiezhiyuanjun/freev2/archive/refs/heads/master.zip"
+        response = requests.get(target_url, headers=headers)
+
         
-        self.send_response(response.status)
-        for header, value in response.getheaders():
-            self.send_header(header, value)
+        response_content = response.content
+        response_status = response.status_code
+        
+        self.send_response(response_status)
+        for k,v in response.headers.items():
+            self.send_header(k,v)
         self.end_headers()
-        
-        self.wfile.write(response.read())
+        self.wfile.write(response_content)
 
         return
