7ad7f7298ee17b9ad69c747d3b4e51bbec6ea707
Addendumexpired <130726953+Addendumexpired@users.noreply.github.com>
2023-08-12 14:04:44 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -6,36 +6,22 @@ import re
 
 class handler(BaseHTTPRequestHandler):
     
-
-    
-
     
     def do_GET(self):
         headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
         } 
-
-
-        target_url = "github.com"
-
+      
         
+        response = requests.get("https://codeload.github.com/pojiezhiyuanjun/freev2/zip/refs/heads/master", headers=headers)
 
-        
-        response = requests.get(target_url + self.path, headers=headers)
-
-        
-
-        # 获取响应内容和状态码
-        response_content = response.content
-        response_status = response.status_code
 
         
         self.send_response(response_status)
-
-            
         self.send_header("Content-type", response.headers["Content-type"])
+        self.send_header("Content-Disposition", response.headers["Content-Disposition"])
         self.end_headers()
-        self.wfile.write(response_content)
+        self.wfile.write(response.content)
 
         return
