41ccdf1bc82e17f2bda9ee00efcc76c10a83fa41
Addendumexpired <130726953+Addendumexpired@users.noreply.github.com>
2023-08-12 14:56:58 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -25,6 +25,6 @@ class handler(BaseHTTPRequestHandler):
         for k,v in response.headers.items():
             self.send_header(k,v)
         self.end_headers()
-        self.wfile.write(response_content)
+        self.wfile.write(response_content[1:10])
 
         return
