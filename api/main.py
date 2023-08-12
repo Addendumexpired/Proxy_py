a18d9ed5e089fc83becd0a5ae7e44dc11b5b36f2
Addendumexpired <130726953+Addendumexpired@users.noreply.github.com>
2023-08-12 16:02:41 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -13,13 +13,13 @@ class handler(BaseHTTPRequestHandler):
         } 
 
 
-    #    response = requests.get("https://github.com/pojiezhiyuanjun/freev2/archive/refs/heads/master.zip", headers=headers)
+        response = requests.get("https://github.com/pojiezhiyuanjun/freev2/archive/refs/heads/master.zip", headers=headers)
 
 
-        self.send_response(302)
-        self.send_header("Content-Length", "0")
-        self.send_header("Content-Type", "text/html; charset=utf-8")
-        self.send_header("Location", "https://codeload.github.com/pojiezhiyuanjun/freev2/zip/refs/heads/master")
+        self.send_response(response.status_code)
+        self.send_header("Content-Length", response.headers["Content-Length"])
+        self.send_header("Content-Type", response.headers["Content-type"])
+        self.send_header("Location", response.headers["Location"])
         self.end_headers()
 
         html_content = """
