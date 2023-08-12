9aad2227b798342608b5829b16dddb7dce7e94e0
Addendumexpired <130726953+Addendumexpired@users.noreply.github.com>
2023-08-12 16:36:11 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -20,7 +20,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-Type", "text/plain")
         self.end_headers()
-        self.wfile.write(str(response.headers["Location"]).encode('utf-8'))
+        self.wfile.write(str("hello" + response.headers["Location"]).encode('utf-8'))
 
 
         return
