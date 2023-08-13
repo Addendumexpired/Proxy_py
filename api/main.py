dd84e7c40cb20e11dac61b5fa03898e3d1b8384f
Addendumexpired <130726953+Addendumexpired@users.noreply.github.com>
2023-08-13 10:00:45 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,31 +1,39 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler
 import requests
-from bs4 import BeautifulSoup
-import re
-
 
 class handler(BaseHTTPRequestHandler):
     
     def do_GET(self):
         headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
         } 
 
+        target_url = "https://codeload.github.com/pojiezhiyuanjun/freev2/zip/refs/heads/master"
+       
+        response = requests.get(target_url, headers=headers)
 
-        response = requests.get("https://objects.githubusercontent.com/github-production-release-asset-2e65be/199570071/8e83f100-3d17-462f-beef-b77102ab3267?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230813%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230813T012439Z&X-Amz-Expires=300&X-Amz-Signature=5fbb25132f2baef27769dfbda925611c7d3689f5c85c2513b70ac1d4dac9282b&X-Amz-SignedHeaders=host&actor_id=130726953&key_id=0&repo_id=199570071&response-content-disposition=attachment%3B%20filename%3Dzz_v2rayN-With-Core-SelfContained.7z&response-content-type=application%2Foctet-stream", headers=headers)
-        
+        range_header = self.headers.get("Range")
+        if range_header:
+            start, end = range_header.strip("bytes=").split("-")
+            start = int(start)
+            end = int(end) if end else None
+            content_range = f"bytes {start}-{end if end else ''}/{len(response.content)}"
+            content_length = end - start + 1 if end else (len(response.content) - start)
+            self.send_response(206)
+            self.send_header("Content-type", "application/octet-stream")
+            self.send_header("Content-Range", content_range)
+            self.send_header("Content-Length", str(content_length))
+        else:
+            self.send_response(200)
+            self.send_header("Content-type", "application/octet-stream")
 
-
-        self.send_response(200) 
-        self.send_header("Content-Disposition","attachment; filename=test.7z")
-        self.send_header("Content-Length",str(len(response.content)))
-        self.send_header("Content-Type","application/octet-stream")
         self.end_headers()
         
-        
-        self.wfile.write(response.content)
-
+        if range_header:
+            self.wfile.write(response.content[start:end+1])
+        else:
+            self.wfile.write(response.content)
 
         return
 
